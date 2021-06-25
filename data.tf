17e54a00e48b142163b99f8e60295f80031191c4
Ryan Tiffany <greyhoundforty@users.noreply.github.com>
2021-06-25 09:14:23 -0500

    Update data.tf

--- data.tf
+++ data.tf
@@ -6,11 +6,3 @@ data "ibm_resource_group" "rg" {
 data "ibm_is_zones" "regional_zones" {
   region = var.region
 }
-
-data "ibm_is_ssh_key" "regional_ssh_key" {
-  name = var.ssh_key
-}
-
-data "ibm_is_image" "default" {
-  name = var.os_image
-}
